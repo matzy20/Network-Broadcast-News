87fafeaa5aa839ae8ef0a2ff6cece34aa8116760
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-02 15:40:49 -1000

    got server broadcasting clients

--- client2.js
+++ client2.js
@@ -1,20 +1,22 @@
 var net = require('net');
-
-
 var PORT = 1337;
+
 var socket = new net.Socket();
 
 socket.setEncoding('utf8');
 
   socket.on('connect', function (){
     console.log('hey I connected ' + PORT);
+    // process.stdin.on('data', function (data){
+    // });
   });
 
-  process.stdin.on('data', function (data){
-  //removed stdout since it's going to server vs terminal
-  //need socket just to write
-  socket.write(data);
+  process.stdin.on('data', function (data) {
+    //removed stdout since it's going to server vs terminal
+    //need socket just to write
+    socket.write(data);
   });
+
   //need to have this for when the server returns/spits back data
   socket.on('data', function (data){
     console.log(data);
--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 var net = require('net');
 
-var PORT = 4567;
+//need server running to have client work
+//make a chat with multiple connections
+
+var PORT = 1337;
 var server = net.createServer(connectionListener);
 
+//made this global so everyone can access
+var allClients = [];
+
 function connectionListener(socket){
-  socket.setEncoding('utf8');
+
+  //this is the very FIRST time socket connects
+  allClients.push(socket);
   console.log('Client connected!');
 
+  socket.setEncoding('utf8');
+  server.getConnections(function (err, length) {
+    console.log(length);
+  });
+
+  //broadcast to all sockets/clients
   socket.on('data', function (data) {
     console.log(data);
+    for (var i =0; i < allClients.length; i++){
+      // console.log("hey you", allClients[i]);
+      allClients[i].write(data);
+    }
+  });
+
+  socket.on('end', function (){
+    console.log('Socket ended');
+    server.getConnections(function (err, length){
+      console.log(length);
+    });
   });
 }
-
 
 server.listen(PORT, function() {
   console.log('Server listening on port ' + PORT);
